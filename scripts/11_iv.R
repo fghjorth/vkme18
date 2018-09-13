98f585665ca91ed5d33743214cd87278e237128f
Frederik Hjorth <frederikhjorth@gmail.com>
2018-09-13 11:36:57 +0200

    new estimatr

--- scripts/11_iv.R
+++ scripts/11_iv.R
@@ -1,6 +1,6 @@
 setwd("~/GitHub/vkme18")
 
-pacman::p_load(tidyverse,haven,estimatr,ivpack)
+pacman::p_load(tidyverse,haven,estimatr)
 
 cs <- read_dta("data/11_colantonestanig.dta")
 
@@ -13,6 +13,7 @@ summary(olsmodel)
 # iv regression
 # ivreg2 leave_share (import_shock=instrument_for_shock) i.nuts1, cluster(nuts2) 
 
-ivmodel <- ivreg(leave_share ~ import_shock + factor(nuts1) |  instrument_for_shock + factor(nuts1) , data=cs)
+ivmodel <- iv_robust(leave_share ~ import_shock + factor(nuts1) |  instrument_for_shock + factor(nuts1), 
+                     clusters = factor(nuts2), data=cs, se_type = "stata")
 
-cluster.robust.se(ivmodel,factor(cs$nuts2))
+summary(ivmodel)
