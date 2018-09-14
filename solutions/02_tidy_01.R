85e69fce5e97f7473fc6b035b3a5329b235cfc2e
Frederik Hjorth <frederikhjorth@gmail.com>
2018-09-14 05:21:24 +0200

    tidy solution 1

---
+++ solutions/02_tidy_01.R
@@ -0,0 +1,44 @@
+rm(list=ls())
+
+library(foreign)
+library(tidytext)
+library(dplyr)
+library(plyr)
+library(ggplot2)
+library(devtools)
+library(tidyr)
+library(magrittr)
+library(stringr)
+library(lubridate)
+library(readxl)
+
+# SPM 1
+trolltweets <- import("https://github.com/fivethirtyeight/russian-troll-tweets/raw/master/IRAhandle_tweets_1.csv")
+names(trolltweets)
+head(trolltweets)
+
+# SPM 2
+table(trolltweets$language)
+trolltweets %>% 
+  dplyr::count(language) %>%
+  arrange(desc(n))
+
+# SPM 3
+table(trolltweets$region)
+trolltweets %>% 
+  group_by(region) %>% 
+  dplyr::summarize(fo = sum(followers)) %>% 
+  arrange(desc(fo))
+
+# SPM 4
+trolltweets %>% 
+  group_by(language) %>% 
+  dplyr::summarize(re = sum(retweet)) %>% 
+  arrange(desc(re))
+
+# SPM 5
+trolltweets %>% 
+  dplyr::summarize(donald = sum(str_count(content, pattern = "Trump")))
+
+trolltweets %>% 
+  dplyr::summarize(hillary = sum(str_count(content, pattern = "Clinton")))
--- solutions/guide_til_upload.md
+++
@@ -1,6 +0,0 @@
-﻿# Guide til upload af løsninger på øvelser
-
-1. Lav øvelsen i et selvindeholdt script, sådan at man kan frembringe resultaterne kun ved evt. at ændre working directory. Henvisninger til data skal henvise med "../data".
-1. Navngiv filen med hhv. et tocifret tal for den undervisningsgang øvelsen vedrører samt dit ku-brugernavn, adskilt af underscore. Frederiks løsning til første undervisningsgang ville fx. hedde `01_kzc744.r`.
-1. Upload filen til mappen "solutions" der ligger i mappen "Files" på Absalon.
-
