02309b594f247a76b66ee9e4b74f99f66efb02ac
Frederik Hjorth <frederikhjorth@gmail.com>
2018-10-10 09:40:33 +0200

    solution

---
+++ solutions/04_text_01.R
@@ -0,0 +1,37 @@
+#Henter pakker
+pacman::p_load(tidyverse,readtext,quanteda,stringr,rio)
+
+#Directory 
+setwd("C:/Users/kzc744/Documents/GitHub/vkme18")
+
+# 1: importer tekster
+nytaars <- readtext("data/04_royal",encoding="latin1")
+
+# 2: lav korpus
+ncorp <- corpus(nytaars)
+
+# 3: Ordbog: Skjulte hentydninger til danske fodboldklubber.  
+fodboldordbog <- dictionary(list(VejleBoldklub = c("jeppe virøe", "vejle", "vb"), 
+                                 FCMidtjylland = c("fcm", "midtjylland", "herning"),
+                                 FCKøbenhavn = c("fck", "hovedstaden", "københavn"), 
+                                 AaB = c("danmarks paris","aab", "aalborg"),
+                                 EsbjergfB = c("esbjerg", "efb"),
+                                 AGF = c("aarhus", "smilets by", "agf"),
+                                 Brøndby = c("brøndby", "vestegnen", "bif"),
+                                 ACHorsens = c("horsens", "ach"), 
+                                 FCNordsjælland = c("peter brixtofte", "farum", "fcn"), 
+                                 Sønderjyske = c("ringridning", "sønderjylland", "haderslev"), 
+                                 RandersFC = c("mokai", "randers", "rfc"), 
+                                 OB = c("h.c. andersen", "odense", "ob"), 
+                                 Vendsyssel = c("thy", "vendsyssel", "vff", "skagen"), 
+                                 HobroIK = c("quincy antipas", "hobro", "hik")))
+
+#Laver en dfm på baggrund af den store fodbold ordbog
+fodbolddfm <- dfm(ncorp,dictionary = fodboldordbog) 
+
+# 5: lav om til data frame
+fodbolddf <- quanteda::convert(fodbolddfm,to="data.frame")
+
+#Udregn frekvens - Jeppe er ikke nævnt en eneste gang i hele 72 taler 
+frekvens <- colSums(fodbolddf != 0)
+frekvens
