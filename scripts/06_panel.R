d238fb79b256c6eab725e73581f1710209571c36
Frederik Hjorth <frederikhjorth@gmail.com>
2018-10-11 14:33:20 +0200

    plm

--- scripts/06_panel.R
+++ scripts/06_panel.R
@@ -1,6 +1,12 @@
 setwd("~/GitHub/vkme18")
 
-pacman::p_load(tidyverse,broom,rio,stargazer,multiwayvcov)
+pacman::p_load(tidyverse,broom,rio,stargazer,plm,lmtest)
+
+#bred til lang form
+widedf <- data_frame(unit=letters[1:3],yt1=c(1,3,6),yt2=c(2,6,7))
+
+tidydf <- widedf %>% 
+  gather(time,value,2:3)
 
 #kigger på filerne i mappen med Mutz' replikationsdata
 dir("data/05_mutz")
@@ -23,16 +29,16 @@ mutz06<-mutz06 %>%
 cm1 <- lm(cutdifftherm~dem+economy+personeco+sdo,data=filter(mutz06,wave==1))
 stargazer(cm1,type="text")
 
-#panelmodel med respondent fixed effects
-pm1 <- lm(cutdifftherm~dem+economy+personeco+sdo+id+wave,data=mutz06)
+#panelmodel med respondent og wave fixed effects
+pm1 <- plm(cutdifftherm~dem+economy+personeco+sdo,data=mutz06,index=c("id","wave"),model="within")
 stargazer(pm1,type="text",omit="id")
 
 #vi kan også se på begge ved siden af hinanden
 stargazer(cm1,pm1,type="text",omit="id")
 
-#hov, en sidste ting: vi skal bruge standardfejl clustered på individniveau
-clustervcov <- multiwayvcov::cluster.vcov(pm1,mutz06$id)
-clusterses <- clustervcov %>% diag() %>% sqrt()
+#hov, en sidste ting: vi skal bruge standardfejl clustered på individniveau (dvs. 'group')
+clustervcov <- coeftest(pm1,vcovHC(pm1,cluster="group"))
+clusterses <- clustervcov[,2]
 
 #vi kigger på modellerne igen, og angiver at vi vil bruge clustered se's for panelmodellen
-stargazer(cm1,pm1,type="text",omit="id",se=list(NULL,clusterses))
+stargazer(cm1,pm1,type="text",se=list(NULL,clusterses))
