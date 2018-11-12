e122b9f9030aef56c1b9b6056af38ed51d4634ff
Frederik Hjorth <frederikhjorth@gmail.com>
2018-11-12 09:03:37 +0100

    multigather ex tweaks

--- scripts/99_multigather.R
+++ scripts/99_multigather.R
@@ -20,13 +20,17 @@ panel_long1 <- panel_wide %>%
 #... men vi har flere holdningsvariable. det nemmeste er nok at samle id'er og tidsinvariante variable i et langt format
 # og så flette de tidsvarierende varibale på een ad gangen.
 
+#først laver vi en lang version med id, de tidsinvariante variable (her: køn), og den første af de tidsvarierende
+
 panel_long1 <- panel_wide %>% 
-  select(id,koen,contains("holdning1")) %>% #contains() samler alle de variabelnavne der indeholder strengen
+  select(id,koen,contains("holdning1")) %>% #contains() samler alle de variabelnavne der indeholder en given streng
   gather(tid,holdning1,contains("holdning1")) %>% 
-  mutate(tid=str_sub(tid,-2)) #lille ekstra trick: m str_sub() tager jeg de sidste 2 tegn af variabelnavnet for at lave en tidsindikator
+  mutate(tid=str_sub(tid,-2)) #lille ekstra trick: m str_sub() tager vi de sidste 2 tegn af variabelnavnet for at lave en tidsindikator
+
+#så laver vi en lang version med hver af de tidsvarierende variable og id
 
 panel_long2 <- panel_wide %>% 
-  select(id,contains("holdning2")) %>% #for de efterflg variable vælger jeg kun id og holdningsvariablen, dvs ikke de tidsinvariante (her køn)
+  select(id,contains("holdning2")) %>% 
   gather(tid,holdning2,contains("holdning2")) %>% 
   select(-tid)
 
@@ -35,4 +39,4 @@ panel_long2 <- panel_wide %>%
 panel_long <- panel_long1 %>% 
   left_join(.,panel_long2,by="id")
 
-#hvis der havde været flere havde sekvensen bare fortsat med left_join(.,panel_long3,by="id") osv osv
+#hvis der havde været flere havde sekvensen bare fortsat med %>% left_join(.,panel_long3,by="id") osv osv
