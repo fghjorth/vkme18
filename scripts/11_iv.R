b55a66f4c1616b007ea01dc0ecfd79df321c30cc
Frederik Hjorth <frederikhjorth@gmail.com>
2018-11-22 09:54:15 +0100

    cs tweaks

--- scripts/11_iv.R
+++ scripts/11_iv.R
@@ -1,6 +1,6 @@
 setwd("~/GitHub/vkme18")
 
-pacman::p_load(tidyverse,haven,estimatr)
+pacman::p_load(tidyverse,haven,estimatr,stargazer)
 
 cs <- read_dta("data/11_colantonestanig.dta")
 
@@ -8,12 +8,20 @@ cs <- read_dta("data/11_colantonestanig.dta")
 # reg leave_share import_shock i.nuts1, cluster(nuts2)
 olsmodel<-lm_robust(leave_share ~ import_shock + factor(nuts1), data = cs, clusters = factor(nuts2), se_type = "stata")
 
-summary(olsmodel)
-
 # iv regression
 # ivreg2 leave_share (import_shock=instrument_for_shock) i.nuts1, cluster(nuts2) 
 
 ivmodel <- iv_robust(leave_share ~ import_shock + factor(nuts1) |  instrument_for_shock + factor(nuts1), 
                      clusters = factor(nuts2), data=cs, se_type = "stata")
 
-summary(ivmodel)
+# stargazer spiser ikke estimatr-output direkte, så vi definerer et lm-objekt og indlæser koefficienter og standardfejl 
+# fra de rigtige modeller i stargazer-kommandoen
+lmmodel <- lm(leave_share ~ import_shock + factor(nuts1), data = cs)
+
+stargazer(lmmodel,lmmodel,
+          coef = list(olsmodel$coefficients,ivmodel$coefficients),
+          se=list(olsmodel$std.error,ivmodel$std.error),
+          type = "text",omit = "factor",
+          column.labels = c("OLS","IV"))
+
+# ØVELSE: estimer model 4 og 6 fra Tabel 1
